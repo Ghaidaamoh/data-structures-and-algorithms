11d85f7adff34c8ff29a3dc097d01e95499113d9
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-09-27 22:55:46 +0300

    challenge32

---
+++ challenge32/linkedlist.js
@@ -0,0 +1,16 @@
+const Node = require("./node");
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insert(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+}
+module.exports = LinkedList;
